a8a4df6f5f82b75469b41ccbacd097bab1d8f53e
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-28 07:00:29 +0530

    Added POst Processing Controller

---
+++ Assets/Scripts/Static/ExtensionFunctions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtensionFunctions
+{
+    public static float Map(float from, float fromMin, float fromMax, float toMin, float toMax)
+    {
+        var fromAbs = from - fromMin;
+        var fromMaxAbs = fromMax - fromMin;
+
+        var normal = fromAbs / fromMaxAbs;
+
+        var toMaxAbs = toMax - toMin;
+        var toAbs = toMaxAbs * normal;
+
+        var to = toAbs + toMin;
+
+        return to;
+    }
+
+    public static string Format2DecimalPlace(float value) => value.ToString("0.##");
+}
