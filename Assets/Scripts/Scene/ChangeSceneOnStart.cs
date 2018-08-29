4a2b13219327ad8628d154e9553ac431b7863adf
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-29 09:36:20 +0530

    Added Scenes Copy and Loader

---
+++ Assets/Scripts/Scene/ChangeSceneOnStart.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnStart : MonoBehaviour
+{
+    public Slider loadingSlider;
+    public float bufferTime = 2f;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        StartCoroutine(LoadNextSceneAsync());
+    }
+
+    IEnumerator LoadNextSceneAsync()
+    {
+        loadingSlider.value = 0;
+        yield return new WaitForSeconds(bufferTime);
+
+        int sceneIndex = NextSceneData.nextSceneIndex;
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            loadingSlider.value = operation.progress;
+            yield return null;
+        }
+    }
+}
