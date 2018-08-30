2b3351ce1176b795db6166311fba102e24e2f5c1
Rudrangshu Nandi <rudrangshu_1997@yahoo.com>
2018-08-30 21:54:33 +0530

    Added Minor Change to Flip Player Based on Mouse

---
+++ Assets/Scripts/UI/FlipSpriteBasedOnMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSpriteBasedOnMouse : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Transform playerTransform;
+    public SpriteRenderer playerRenderer;
+    public float minOffset = 0.5f;
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        float mouseX = mainCamera.ScreenToWorldPoint(Input.mousePosition).x;
+        float playerX = playerTransform.position.x;
+
+        if (mouseX < playerX - minOffset)
+            playerRenderer.flipX = true;
+        else if (mouseX > playerX + minOffset)
+            playerRenderer.flipX = false;
+    }
+}
